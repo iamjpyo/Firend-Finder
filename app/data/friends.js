c05bff0a5f87b79ffc65bed0b7bb4714223dd3b5
Juan Pablo <jpluncumilla@gmail.com>
2019-08-18 22:58:52 -0400

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Cameron Giles",
+		photo: "https://uproxx.com/wp-content/uploads/2016/11/camron-feat-uproxx.jpg",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Curtis Jackson",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSenLO4gozoIe20pcvP6z_4EVQKT2GUhvt2TR5g596x052ISCytzg",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Marshall Mathers",
+		photo: "https://www.snakkle.com/wp-content/uploads/2012/10/eminem-yearbook-high-school-young-1989-photo-GC.jpg",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
